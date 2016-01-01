6d5519a2030c041dd9e29eb5737bd705352147e0
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2016-01-01 20:04:30 +0000

    Happy New Year

--- branches/SM-1_4-STABLE/squirrelmail/src/configtest.php
+++ branches/SM-1_4-STABLE/squirrelmail/src/configtest.php
@@ -4,7 +4,7 @@ echo "Disallowed"; die;
 /**
  * SquirrelMail configtest script
  *
- * @copyright 2003-2015 The SquirrelMail Project Team
+ * @copyright 2003-2016 The SquirrelMail Project Team
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id$
  * @package squirrelmail
